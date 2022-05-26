f15d4c70152cbb33e1d51c125a191079e3c4f8ff
SebaChuaqui <schuaquicano@gmail.com>
2022-05-25 21:54:19 -0300

    Objetos VII

--- index.js
+++ index.js
@@ -12,57 +12,59 @@ function Paciente(nombre, edad, rut, diagnostico) {
     var _edad = edad;
     var _rut = rut;
     var _diagnostico = diagnostico;
+
+    Object.defineProperty(this, '_getNombre', {
+        get: function () {
+            return _nombre;
+        }
+    });
+
+    Object.defineProperty(this, '_setNombre', {
+        set: function (nombre) {
+            _nombre = nombre;
+        }
+    });
+
+    Object.defineProperty(this, '_getEdad', {
+        get: function () {
+            return _edad;
+        }
+    });
+
+    Object.defineProperty(this, '_setEdad', {
+        set: function (edad) {
+            _edad = edad;
+        }
+    });
+
+    Object.defineProperty(this, '_getRut', {
+        get: function () {
+            return _rut;
+        }
+    });
+
+    Object.defineProperty(this, '_setRut', {
+        set: function (rut) {
+            _rut = rut;
+        }
+    });
+
+    Object.defineProperty(this, '_getDiagnostico', {
+        get: function () {
+            return _diagnostico;
+        }
+    });
+
+    Object.defineProperty(this, '_setDiagnostico', {
+        set: function (diagnostico) {
+            _diagnostico = diagnostico;
+        }
+    });
+
 }
 
 // Métodos Object DefineProperty
 
-Object.defineProperty(this, '_getNombre', {
-    get: function () {
-        return _nombre;
-    }
-});
-
-Object.defineProperty(this, '_setNombre', {
-    set: function (nombre) {
-        _nombre = nombre;
-    }
-});
-
-Object.defineProperty(this, '_getEdad', {
-    get: function () {
-        return _edad;
-    }
-});
-
-Object.defineProperty(this, '_setEdad', {
-    set: function (edad) {
-        _edad = edad;
-    }
-});
-
-Object.defineProperty(this, '_getRut', {
-    get: function () {
-        return _rut;
-    }
-});
-
-Object.defineProperty(this, '_setRut', {
-    set: function (rut) {
-        _rut = rut;
-    }
-});
-
-Object.defineProperty(this, '_getDiagnostico', {
-    get: function () {
-        return _diagnostico;
-    }
-});
-
-Object.defineProperty(this, '_setDiagnostico', {
-    set: function (diagnostico) {
-        _diagnostico = diagnostico;
-    }
-});
 
 // Getters And Setters
 
@@ -115,6 +117,7 @@ Consultorio.prototype.mostrarPacientes = function () {
 Consultorio.prototype.buscarInfoxNombre = function (nombre) {
 
     // Ciclo FOR
+
     for (var i = 0; i < this.pacientes.length; i++) {
         if (nombre == this.pacientes[i].getNombre()) {
             console.log('Buscar información del paciente por nombre: ' + this.pacientes[i].getNombre());
@@ -124,8 +127,13 @@ Consultorio.prototype.buscarInfoxNombre = function (nombre) {
 
 }
 
+var paciente1 = new Paciente('Juan', 55, '14.346.722-8', "Hipotiroidismo");
+var paciente2 = new Paciente('Rosa', 44, '18.126.790-3', "Cancer de Mama");
+var paciente3 = new Paciente('Monica', 49, '15.378.325-1', "Hipertensión");
+var consultorio1 = new Consultorio('Consultorio Simpsonito', [paciente1, paciente2, paciente3])
 
-
+consultorio1.mostrarPacientes();
+consultorio1.buscarInfoxNombre('Monica');
 
 
 
